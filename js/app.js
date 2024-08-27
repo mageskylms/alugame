1df88b6388fe39b9147a52c1f8249c26464cf987
Lucas Magesky <34316405+mageskylms@users.noreply.github.com>
2024-08-27 19:01:31 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let jogosAlugados = 1;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+function alterarStatus(id) {
+    let game = document.getElementById(`game-${id}`);
+    let imagem = game.querySelector(`.dashboard__item__img`);
+    let botao = game.querySelector(`.dashboard__item__button`);
+
+    if (imagem.classList.contains(`dashboard__item__img--rented`)) {
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${game.textContent}?`)) {
+            imagem.classList.remove(`dashboard__item__img--rented`);
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
+    } else {
+        imagem.classList.add(`dashboard__item__img--rented`);
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+        jogosAlugados++;
+    }
+    contarEExibirJogosAlugados();
+}
+
+// Inicializa a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
